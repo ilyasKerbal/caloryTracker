8a49a506cb2fb47bec38f44ff75973e164599321
ILYAS KERBAL <kerbalsc@gmail.com>
2022-07-02 21:18:15 +0100

    Dagger-Hilt Setup with SharedPrefs

---
+++ app/src/main/java/io/github/ilyaskerbal/calorytracker/CaloryTrackerApp.kt
@@ -0,0 +1,8 @@
+package io.github.ilyaskerbal.calorytracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CaloryTrackerApp : Application() {
+}
---
+++ app/src/main/java/io/github/ilyaskerbal/calorytracker/DI/AppModule.kt
@@ -0,0 +1,30 @@
+package io.github.ilyaskerbal.calorytracker.DI
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.ilyaskerbal.core.data.preferences.DefaultPreferences
+import io.github.ilyaskerbal.core.domain.preferences.Preferences
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ) : SharedPreferences = app.getSharedPreferences("shared_pref", Context.MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun providePreferences(
+        sharedPreferences: SharedPreferences
+    ) : Preferences = DefaultPreferences(sharedPreferences)
+
+}
